8cb7af4effce739cd5f8d5081925ddeb33f858dc
Sudhanshu Gupta <sudhanshy=uvinodgupta@gmail.com>
2018-06-26 22:29:04 +0530

    Updated the readme file and added javadoc comments

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # csv-loader
-A library to load csv file data into List/Stream of Objects
+A tool to load csv file data into List/Stream of Objects
+
+## How to Use
+	1. Create an interface with getter methods for the fields you want to extract from the csv file. No implementation of the interface required.
+	
+			public interface Employee {
+	
+				@FieldIndex(0)
+				public long getEmployeeId();
+				
+				@FieldIndex(1)
+				public String getFirstName();
+				
+				@FieldIndex(2)
+				public String getLastName();
+				
+				@FieldIndex(3)
+				public double getCTC();
+				
+				@FieldIndex(4)
+				public boolean isPermanent();
+				
+				@FieldIndex(5)
+				public char getGrade();
+				
+				@FieldIndex(6)
+				public Date getLastSeen(Parser<Date> parser);
+				
+				@FieldIndex(7)
+				public int getExperience();
+				
+			}
+		
+	2. Mark the methods with **@FieldIndex** with 0 based index of the field in the csv file. 
+	
+	3. Create an instance of CsvLoader class using its Builder.
+	
+	4. Use one of the overridden methods to load the data in your csv file into List or Stream of Objects of your interface type.
+	
+	5. Bingo!!! You are ready to operate on your csv data. 
+		
+	
--- src/main/java/com/sidutils/csvloader/CsvLoader.java
+++ src/main/java/com/sidutils/csvloader/CsvLoader.java
@@ -18,6 +18,17 @@ import java.util.stream.Stream;
 
 import lombok.Getter;
 
+/**
+ * Tool to load data from csv file into List/Stream of dynamically generated objects.
+ * Follow the below steps to use it.
+ * <ol>
+ * 	<li>Create an interfae with getter methods mapped to fields in the csv file with {@link @FieldIndex(<field's column number in the file 0 based>)}</li>
+ * 	<li>Initialize an object of CsvLoader using its Builder</li>
+ * 	<li>Call one of the overridden methods to get the List/Stream of your interface type</li>
+ * </ol>
+ * @author sudhanshu-g
+ *
+ */
 @Getter
 public class CsvLoader {
 
@@ -29,6 +40,11 @@ public class CsvLoader {
 
 	}
 
+	/**
+	 * Builder class to create CsvLoader instance
+	 * @author sudhanshu-g
+	 *
+	 */
 	public static class Builder {
 		private CsvLoader instance;
 
@@ -36,26 +52,57 @@ public class CsvLoader {
 			instance = new CsvLoader();
 		}
 
+		/**
+		 * Delimeter of the csv file fields. Default is comma(,). Use this method to change it.
+		 * @param regex delimeter that splits the fields in the csv file. Supports regex 
+		 * @return The Builder instance for method chaining
+		 */
 		public Builder delimeter(String regex) {
 			instance.delimiter = regex;
 			return this;
 		}
 		
+		/**
+		 * Skip reading the file header (the first line) in the csv file.
+		 * Default: True
+		 * @param skip turn it off if your csv file does not have a header line
+		 * @return The Builder instance for method chaining
+		 */
 		public Builder skipHeader(boolean skip) {
 			instance.skipHeader = skip;
 			return this;
 		}
 		
+		/**
+		 * 
+		 * @return the CsvLoader instance.
+		 */
 		public CsvLoader build() {
 			return instance;
 		}
 	}
 
+	/**
+	 * Load the csv data as List of Objects 
+	 * @param fileName Absolute file path of the csv file
+	 * @param type The interface class
+	 * @see com.sidutils.csvloader.CsvLoader
+	 * @return List of dynamically generated objects from the interface supplied
+	 * @throws FileNotFoundException
+	 */
 	public <T> List<T> loadCsv(String fileName, Class<T> type) throws FileNotFoundException {
 		File file = new File(fileName);
 		return loadCsv(new FileInputStream(file),type);
 	}
 	
+	/**
+	 * Load the csv data as List of Objects 
+	 * @param in InputStream for the csv file
+	 * @param type The interface class
+	 * @see com.sidutils.csvloader.CsvLoader
+	 * @return List of dynamically generated objects from the interface supplied
+	 * @throws FileNotFoundException
+	 */
 	public <T> List<T> loadCsv(InputStream in, Class<T> type) throws FileNotFoundException {
 		
 		List<T> list = new ArrayList<>();
@@ -74,15 +121,32 @@ public class CsvLoader {
 		return list;
 	}
 	
+	/**
+	 * Load the csv data as Stream of Objects 
+	 * @param in InputStream for the csv file
+	 * @param type The interface class
+	 * @see com.sidutils.csvloader.CsvLoader
+	 * @return Stream of dynamically generated objects from the interface supplied
+	 * @throws FileNotFoundException
+	 */
 	public <T> Stream<T> loadCsvStream(InputStream in, Class<T> type) throws IOException {
 		Stream<String> lines = new BufferedReader(new InputStreamReader(in, Charset.forName("UTF-8"))).lines();
 		return loadStream(lines, type);
 	}
 
+	/**
+	 * Load the csv data as Stream of Objects 
+	 * @param fileName Absolute file path of the csv file
+	 * @param type The interface class
+	 * @see com.sidutils.csvloader.CsvLoader
+	 * @return Stream of dynamically generated objects from the interface supplied
+	 * @throws FileNotFoundException
+	 */
 	public <T> Stream<T> loadCsvStream(String fileName, Class<T> type) throws IOException {
 		Stream<String> lines = Files.lines(Paths.get(fileName));
 		return loadStream(lines, type);
 	}
+	
 	
 	private <T> Stream<T> loadStream(Stream<String> lines, Class<T> type) {
 		int skipLines = (skipHeader)?1:0;
